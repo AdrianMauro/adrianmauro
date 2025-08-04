cad34b1d7407834a9196c3d710cccffdf22dbdd2
AdrianMauro <acegoes.am@gmail.com>
2025-08-03 20:39:04 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-  document.querySelectorAll('.fade-in').forEach(section => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 100) {
-      section.classList.add('show');
-    }
-  });
-});
